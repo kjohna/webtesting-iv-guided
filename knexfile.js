2b7579a1a46e38ee7ef12c4234fd02534845b5d3
kjohna <43828763+kjohna@users.noreply.github.com>
2019-03-07 11:56:51 -0700

    add config for pg db

--- knexfile.js
+++ knexfile.js
@@ -1,5 +1,12 @@
 // Update with your config settings.
-
+const localPgConnection = {
+  // this is dummy data since there is no pg db set up
+  host: 'localhost', // or addy of pg server
+  database: 'hobbits', // can name the db something diff from sqlite version
+  user: 'student',
+  password: 'hired'
+}
+const prodDbConnection = process.env.DATABASE_URL || localPgConnection;
 module.exports = {
   development: {
     client: 'sqlite3',
@@ -14,6 +21,7 @@ module.exports = {
       directory: './data/seeds',
     },
   },
+  // local config
   testing: {
     client: 'sqlite3',
     connection: {
@@ -27,4 +35,15 @@ module.exports = {
       directory: './data/seeds',
     },
   },
+  // heroku config
+  production: {
+    client: 'pg',
+    connection: prodDbConnection,   // an obj or string
+    migrations: {
+      directory: './data/migrations',
+    },
+    seeds: {
+      directory: './data/seeds',
+    },
+  }
 };
